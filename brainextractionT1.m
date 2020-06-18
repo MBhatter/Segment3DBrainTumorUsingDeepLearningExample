3d61a932c0c86be7d61b626ab12b514e1cd86d97
david fuentes <fuentesdt@gmail.com>
2020-06-18 06:17:45 -0500

    WIP: template

---
+++ brainextractionT1.m
@@ -0,0 +1,20 @@
+%% 3-D Brain Extraction from MRI
+%    Train and cross validate a 3-D U-net for brain extraction on T1 image
+%% load nifti data 
+%   manu -
+%          load nifti data from /rsrch1/ip/egates1/NFBS\ Skull\ Strip/NFBSFilepaths.csv 
+%          into matlab data structure
+
+%% setup data for k-fold cross validation
+%   aurian - split the data into training/ validation/  test  sets
+
+
+%% load the 3D U-net structure
+%   priya - insert code for the 3D Unet
+
+%% train the model on the training set for each fold in the k-fold
+
+%% evaluate the average dice similarity
+%   manu - output nifti files of the test set predictions
+
+%% compare to MONSTR
