08ba9f561c5ddca4e17fa9279c8634bf98e0d616
MBhatter <67134923+MBhatter@users.noreply.github.com>
2020-07-03 22:06:49 -0500

    Manu - Update 7/3/2020 - Latest
    Please use this one to make the changes, the previous update had a few minor problems

--- brainextractionT1.m
+++ brainextractionT1.m
@@ -10,40 +10,127 @@ clear all
 close all
 
 % Read file pathways into table
-folder = '/rsrch1/ip/egates1/NFBS Skull Strip/';
-fullFileName = fullfile(folder, 'NFBSFilepaths.csv')
-T = readtable(fullFileName, 'Delimiter', ',')
+fullFileName = input("Please enter the file pathway: ", 's')
+    % enter: = /rsrch1/ip/egates1/NFBS Skull Strip/NFBSFilepaths.csv
 
-% convert table to cell array
-A = table2array(T)
+delimiter = input("Please enter the delimiter: ", 's')
+    % enter: ,
 
-% create cell arrays to hold Volumetric data
-T1RAI{125,1}=[];
-maskRAI{125,1}=[];
-T1{125,1}=[];
-mask{125,1}=[];
+T = readtable(fullFileName, 'Delimiter', delimiter);
+A = table2array(T);
+volCol = input("Please enter column number for volumetric data: ")
+    % enter: 4
+    
+lblCol = input ("Please enter the column number for mask data: ")
+    % enter: 5
 
-% niftiread 
-% loop through T1RAI column and do niftiread
-for row = 1:125
-    T1RAI{row,1} = niftiread(A{row,2});
-end
+volLoc = A(:,volCol);
+lblLoc = A(:,lblCol);
 
-% loop through maskRAI column and do niftiread to store volumetric data
-for row = 1:125
-    maskRAI{row,1} = niftiread(A{row,3});
-end
+% for user-defined: destination = input("Please enter the file pathway for folder to store training, validation, and test sets: ", 's')
+destination = fullfile(tempdir,'brainTest', 'preprocessedDataset');
 
-% loop through T1 column and do niftiread to read vol data
-for row = 1:125
-    T1{row,1} = niftiread(A{row,4});
-end
+ % define readers
+ maskReader = @(x) (niftiread(x)>0);
+ volReader = @(x) niftiread(x);
+ 
+ %read data into datastores
+ volds = imageDatastore(volLoc, ...
+     'FileExtensions','.gz','ReadFcn',volReader);
+ 
+ classNames = ["background","brain"];
+  pixelLabelID = [0 1];
+ 
+ % read data intp pixelLabeldatastore
+ pxds = pixelLabelDatastore(lblLoc,classNames, pixelLabelID, ...
+        'FileExtensions','.gz','ReadFcn',maskReader);
+  reset(volds);
+  reset(pxds);      
+ 
+   % create directories to store data sets
+        mkdir(fullfile(destination,'imagesMain'));
+        mkdir(fullfile(destination,'labelsMain'));
+        
+        imDir = fullfile(destination, 'imagesMain', 'brainTest');
+        labelDir = fullfile(destination, 'labelsMain', 'brainTest');
+    
 
-% loop through mask column and do niftiread to read in vol data
-for row = 1:125
-    mask{row,1} = niftiread(A{row,5});
-end
+       
+   %% Crop relevant region
+    NumFiles = length(pxds.Files);
+    id = 1;
+    while hasdata(pxds)
+        outL = readNumeric(pxds);
+        outV = read(volds);
+        temp = outL>0;
+        sz = size(outL);
+        reg = regionprops3(temp,'BoundingBox');
+        tol = 64;
+        ROI = ceil(reg.BoundingBox(1,:));
+        ROIst = ROI(1:3) - tol;
+        ROIend = ROI(1:3) + ROI(4:6) + tol;
 
+        ROIst(ROIst<1)=1;
+        ROIend(ROIend>sz)=sz(ROIend>sz);
+
+        tumorRows = ROIst(2):ROIend(2);
+        tumorCols = ROIst(1):ROIend(1);
+        tumorPlanes = ROIst(3):ROIend(3);
+
+        tcropVol = outV(tumorRows,tumorCols, tumorPlanes);
+        tcropLabel = outL(tumorRows,tumorCols, tumorPlanes);
+
+
+% Data set with a valid size for 3-D U-Net (multiple of 8)
+        ind = floor(size(tcropVol)/8)*8;
+        incropVol = tcropVol(1:ind(1),1:ind(2),1:ind(3));
+        mask = incropVol == 0;
+
+%%%%%%%% channelWisePreProcess
+        % As input has 4 channels (modalities), remove the mean and divide by the
+        % standard deviation of each modality independently.
+        incropVol1=single(incropVol);
+
+        chn_Mean = mean(incropVol1,[1 2 3]);
+        chn_Std = std(incropVol1,0,[1 2 3]);
+        cropVol1 = (incropVol1 - chn_Mean)./chn_Std;
+
+        rangeMin = -5;
+        rangeMax = 5;
+        % Remove outliers
+        cropVol1(cropVol1 > rangeMax) = rangeMax;
+        cropVol1(cropVol1 < rangeMin) = rangeMin;
+
+        % Rescale the data to the range [0, 1]
+        cropVol1 = (cropVol1 - rangeMin) / (rangeMax - rangeMin);
+
+%%%%%%%%        
+        % Set the nonbrain region to 0
+        cropVol1(mask) = 0;
+        cropLabel = tcropLabel(1:ind(1),1:ind(2),1:ind(3))
+
+        
+        % save preprocessed data to folders
+         save([imDir num2str(id,'%.3d') '.mat'],'cropVol1');
+         save([labelDir num2str(id,'%.3d') '.mat'],'cropLabel');
+         id=id+1;
+
+   end  
+    
+
+%% create datastores for processed labels and images
+
+% procvolds stores processed T1 volumetric data
+procvolReader = @(x) matRead(x);
+procvolLoc = fullfile(destination,'imagesMain');
+procvolds = imageDatastore(procvolLoc, ...
+    'FileExtensions','.mat','ReadFcn',procvolReader);
+
+% proclblds stores processed mask volumetric data
+proclblReader = @(x) matRead(x);
+proclblLoc = fullfile(destination,'labelsMain');
+proclblds = imageDatastore(proclblLoc, ...
+    'FileExtensions','.mat','ReadFcn',proclblReader);
 
 
 %% setup data for k-fold cross validation
