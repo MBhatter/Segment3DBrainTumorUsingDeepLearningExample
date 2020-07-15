d77f8378e86e57681105b7936587b562f71e2b55
David Fuentes <fuentesdt@gmail.com>
2020-07-15 09:29:04 -0500

    WIP: @mbhatter

---
+++ applymodel.m
@@ -0,0 +1,14 @@
+clc
+clear all
+close all
+
+%% read setup parameters
+
+%% load nifti file
+
+%% load trained network
+
+%% apply trained network to nifti image
+
+%% write output to disk as a nifti file
+
