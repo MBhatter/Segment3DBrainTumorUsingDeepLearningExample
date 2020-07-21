e3001b4a3054c90bcf2d7f7eb2edf19b26d6e58b
David Fuentes <fuentesdt@gmail.com>
2020-07-21 09:36:14 -0500

    WIP: organizing

--- ImageSegmentationDeepMedic.m
+++
@@ -1,7 +0,0 @@
-classdef ImageSegmentationDeepMedic  < ImageSegmentationBaseClass  
-   methods
-      function loadneuralnet(obj,NumberChannels)
-      % TODO - @amaleki101
-      end
-   end
-end
--- ImageSegmentationDensenet2D.m
+++
@@ -1,7 +0,0 @@
-classdef ImageSegmentationDensenet2D  < ImageSegmentationBaseClass  
-   methods
-      function loadneuralnet(obj,NumberChannels)
-      % TODO - @psarlashkar
-      end
-   end
-end
